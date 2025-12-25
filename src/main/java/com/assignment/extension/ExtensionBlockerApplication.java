1dcd722b73a7f48a0e9cc015a18df345fed74000
SinnoLn <sintmtn69@gmail.com>
2025-12-25 23:43:22 +0900

    chore: 프로젝트 초기 환경 설정

---
+++ src/main/java/com/assignment/extension/ExtensionBlockerApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionBlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensionBlockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/extension/ExtensionBlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.extension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionBlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
